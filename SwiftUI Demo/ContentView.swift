a277e260cf12d6d6095e2a0ae6d171fb227b7c50
Roman Bespalov <voordex@gmail.com>
2022-12-18 17:45:26 +0300

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Роман Беспалов on 18.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI Demo
+//
+//  Created by Роман Беспалов on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
